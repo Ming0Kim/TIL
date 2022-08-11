929686f78faae25d0bddc937af58eda85fa2b7f7
Ming0Kim <minofficial13@gmail.com>
2022-08-11 11:10:35 +0900

    부분집합

--- 오답노트/불가능을/부분집합생성.py
+++ 오답노트/불가능을/부분집합생성.py
@@ -5,3 +5,15 @@ for i in num:
     for j in range(n):
         subnum += [(subnum[j] + [i])]
 print(subnum)
+
+A = 12
+arr = [i for i in range(1, A+1)]
+result=[]
+
+# 비트를 이용한 부분집합 생성
+for i in range(1<<len(arr)):
+    subset = []
+    for j in range(len(arr)):
+        if i & (1<<j):
+            subset += [arr[j]]
+    
