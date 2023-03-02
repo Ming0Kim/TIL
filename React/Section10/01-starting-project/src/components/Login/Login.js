bd8c923d4cadad0eb920c77bd0f903c0559862e1
Ming0Kim <minofficial13@gmail.com>
2023-03-03 00:59:56 +0900

    Section 10: useReducer()

--- React/Section10/01-starting-project/src/components/Login/Login.js
+++ React/Section10/01-starting-project/src/components/Login/Login.js
@@ -1,48 +1,81 @@
-import React, { useState, useEffect } from 'react';
+// [1] import useReducer
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+// [3] emailReducer 함수
+// 컴포넌트 함수의 범위 밖에서 만들어질 수 있음
+// 컴포넌트 함수 내부에서 정의된 그 어떤 것과도 상호 작용할 필요가 없기 때문
+// [9]
+// [11]
+const emailReducer = (state, action) => {
+  if (action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: action.val.includes('@') };
+  }
+  if (action.type === 'INPUT_BLUR') {
+    return { value: state.value, isValid: state.value.includes('@') };
+  }
+  return { value: '', isValid: false };
+};
+
 const Login = props => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  useEffect(() => {
-    console.log('EFFECT RUNNING');
+  // [2] useReducer 호출
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '',
+    isValid: false,
   });
 
   useEffect(() => {
-    const identifier = setTimeout(() => {
-      console.log('Checking form validity');
-      setFormIsValid(
-        enteredEmail.includes('@') && enteredPassword.trim().length > 6,
-      );
-    }, 500);
+    console.log('EFFECT RUNNING');
 
     return () => {
-      console.log('Clean up!');
-      clearTimeout(identifier);
+      console.log('EFFECT CLEANUP');
     };
-  }, [enteredEmail, enteredPassword]);
+  }, [enteredPassword]);
+
+  // useEffect(() => {
+  //   const identifier = setTimeout(() => {
+  //     console.log('Checking form validity');
+  //     setFormIsValid(
+  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
+  //     );
+  //   }, 500);
+
+  //   return () => {
+  //     console.log('Clean up!');
+  //     clearTimeout(identifier);
+  //   };
+  // }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = event => {
-    setEnteredEmail(event.target.value);
+    // [8] dispatchEmail 호출하여 업데이트
+    dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
+
+    setFormIsValid(
+      event.target.value.includes('@') && enteredPassword.trim().length > 6
+    );
   };
 
   const passwordChangeHandler = event => {
     setEnteredPassword(event.target.value);
 
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@'),
-    );
+    // [4] emailState 코드에서 사용
+    setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
 
+  // [10] dispatchEamil
+  // input이 focus를 잃었다만 중요
+  // 추가해야하는 데이터 없음
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({ type: 'INPUT_BLUR' });
   };
 
   const validatePasswordHandler = () => {
@@ -51,7 +84,8 @@ const Login = props => {
 
   const submitHandler = event => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    // [5] submitHandler에 값 전달
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -59,14 +93,16 @@ const Login = props => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            // [6] emailState.isValid
+            emailState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            // [7]
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
