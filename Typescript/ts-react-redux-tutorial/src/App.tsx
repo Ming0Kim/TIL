e7a04719cd98394aab898591f83f91f9013f94e9
Ming0Kim <minofficial13@gmail.com>
2023-05-03 15:46:16 +0900

    [Add] ts-react-redux-tutorial

---
+++ Typescript/ts-react-redux-tutorial/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import CounterContainer from './containers/CounterContainer';
+import './App.css';
+
+const App: React.FC = () => {
+	return <CounterContainer />;
+};
+
+export default App;
---
+++ Typescript/ts-react-redux-tutorial/src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer);
+
+const root = ReactDOM.createRoot(
+	document.getElementById('root') as HTMLElement
+);
+root.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
